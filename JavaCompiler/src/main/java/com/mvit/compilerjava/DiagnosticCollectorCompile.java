3fb2041cba3a17472f69a6078fc8f72f152a0f99
Victor E. Márquez Flores <victor.marquez.flores@gmail.com>
2020-01-30 17:47:10 -0600

    Java Compiler 17:47

---
+++ JavaCompiler/src/main/java/com/mvit/compilerjava/DiagnosticCollectorCompile.java
@@ -0,0 +1,30 @@
+/* 
+ * funciona con jdk
+ */
+
+package com.mvit.compilerjava;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
+
+public class DiagnosticCollectorCompile {
+
+  public static void main(String args[]) throws IOException {
+    JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+    DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
+    StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null);
+    Iterable<? extends JavaFileObject> compilationUnits = fileManager
+        .getJavaFileObjectsFromStrings(Arrays.asList("C:/lenguaje/pruebas_java/JavaCompiler/src/main/java/com/mvit/compilerjava/Test.java"));
+    JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, diagnostics, null,
+        null, compilationUnits);
+    boolean success = task.call();
+    fileManager.close();
+    System.out.println("Success: " + success);
+  }
+}
---
+++ JavaCompiler/src/main/java/com/mvit/compilerjava/Test.java
@@ -0,0 +1,10 @@
+package com.mvit.compilerjava;
+
+import java.util.Date;
+
+public class Test {
+	public static void main(String args[]) {
+	    System.out.println("Hora actual "+ new Date());
+	  }
+
+}
