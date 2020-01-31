db1bbad5623bee6e6d7f157b818e80a59731243c
Victor E. Márquez Flores <victor.marquez.flores@gmail.com>
2020-01-30 18:18:05 -0600

    Java Compiler 30012020 18:17

--- JavaCompiler/src/main/java/com/mvit/compilerjava/Test.java
+++ JavaCompiler/src/main/java/com/mvit/compilerjava/Test.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class Test {
 	public static void main(String args[]) {
-	    System.out.println("Hora actual "+ new Date());
+	    System.out.println("Hora actual es "+ new Date());
 	  }
 
 }
